ebb9337c2307ec74340fe447d57425c0ae657ffe
Robert Varga <115423537+Robi3554@users.noreply.github.com>
2023-02-26 19:45:00 +0200

    Update Main.java

--- Year 2/Sem-2/Lab_JAVA/Lab_1/Ex_4/Main.java
+++ Year 2/Sem-2/Lab_JAVA/Lab_1/Ex_4/Main.java
@@ -5,10 +5,9 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random r = new Random();
-        int low = 0;
         int high = 30;
-        int nr1 = r.nextInt(high - low) + low;
-        int nr2 = r.nextInt(high - low) + low;
+        int nr1 = r.nextInt(high);
+        int nr2 = r.nextInt(high);
 
         System.out.print("Numarul 1: " + nr1);
         System.out.print("\nNumarul 2: " + nr2);
